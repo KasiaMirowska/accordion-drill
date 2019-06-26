7849d73b61cabeab998c85480086489b1e6823fb
Kasia <kasia@miroartdesign.com>
2019-06-26 14:10:17 -0400

    almost all tests done

--- src/Accordion.test.js
+++ src/Accordion.test.js
@@ -21,34 +21,41 @@ const tabSections = [
   ]
 
 describe('Accordion component', () => {
-it ('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Accordion />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    it ('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Accordion />, div);
+        ReactDOM.unmountComponentAtNode(div);
 
-})
+    })
 
-it('renders li empty given section props', () => {
-        const wrapper = shallow(<Accordion />);
+    it('renders li empty given no section props', () => {
+            const wrapper = shallow(<Accordion />);
+            expect(toJson(wrapper)).toMatchSnapshot();
+    })
+
+    it( 'renders no sections by default', () => {
+        const wrapper = shallow(<Accordion default={tabSections.length === 0} />);
         expect(toJson(wrapper)).toMatchSnapshot();
-})
+    })
 
-it( 'renders no sections by default', () => {
-    const wrapper = shallow(<Accordion default={tabSections.length === 0} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
-})
+    it( 'renders a clicked section', () => {
+        const wrapper = shallow(<Accordion sections={tabSections} />);
+        wrapper.find('button').at(0).simulate('click');
+        expect(wrapper.find('p').exists()).toBe(true);
+    })
 
-it( 'renders a clicked section', () => {
-    const wrapper = shallow(<Accordion section={tabSections} />);
-    wrapper.find('button').at(1).simulate('click');
-    expect(toJson(wrapper)).toMatchSnapshot();
-})
+    it( 'renders only the last clicked when multiple sections are clicked', () => {
+        const wrapper = shallow(<Accordion sections={tabSections} />);
+        wrapper.find('button').at(1).simulate('click');
+        wrapper.find('button').at(0).simulate('click')
+        wrapper.find('button').at(2).simulate('click');
+      
+        expect(wrapper.find('tabSection.content')); 
+        console.log(wrapper.debug());
+       
+      });
+    })
 
-it( 'renders only the last clicked when multiple sections are clicked', () => {
-    const wrapper = shallow(<Accordion section={tabSections} />);
-   
-})
-})
 // The component renders no sections as active by default
 // The component opens a clicked section
 // The component only opens the last section when multiple sections have been clicked.
