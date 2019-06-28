355df91f0514b0ae5fe570fe17cd9c75fb3a9752
Kasia <kasia@miroartdesign.com>
2019-06-28 16:36:22 -0400

    tests finished

--- src/Accordion.test.js
+++ src/Accordion.test.js
@@ -18,7 +18,7 @@ const tabSections = [
       title: 'Section 3',
       content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
     },
-  ]
+];
 
 describe('Accordion component', () => {
     it ('renders without crashing', () => {
@@ -26,35 +26,32 @@ describe('Accordion component', () => {
         ReactDOM.render(<Accordion />, div);
         ReactDOM.unmountComponentAtNode(div);
 
-    })
+    });
 
     it('renders li empty given no section props', () => {
             const wrapper = shallow(<Accordion />);
             expect(toJson(wrapper)).toMatchSnapshot();
-    })
+    });
 
     it( 'renders no sections by default', () => {
         const wrapper = shallow(<Accordion default={tabSections.length === 0} />);
         expect(toJson(wrapper)).toMatchSnapshot();
-    })
+    });
 
     it( 'renders a clicked section', () => {
         const wrapper = shallow(<Accordion sections={tabSections} />);
         wrapper.find('button').at(0).simulate('click');
         expect(wrapper.find('p').exists()).toBe(true);
-    })
+    });
 
-    it( 'renders only the last clicked when multiple sections are clicked', () => {
+    it.only('renders only the last clicked when multiple sections are clicked', () => {
         const wrapper = shallow(<Accordion sections={tabSections} />);
         wrapper.find('button').at(1).simulate('click');
-        wrapper.find('button').at(0).simulate('click')
-        wrapper.find('button').at(2).simulate('click');
-      
-        expect(wrapper.find('tabSection.content')); 
-        console.log(wrapper.debug());
-       
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+        expect(wrapper.find('p').text()).toBe(tabSections[1].content); 
       });
-    })
+    });
 
 // The component renders no sections as active by default
 // The component opens a clicked section
